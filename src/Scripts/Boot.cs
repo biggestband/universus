1aa3aa8d4fef4ba55d5ca0c7f83f6267213525b6
greatbrick <speharley@gmail.com>
2025-07-09 17:34:43 -0400

    Army Counts Synced
    Use '[' and ']' after inputting the text fields on the client to test.

--- src/Scripts/Boot.cs
+++ src/Scripts/Boot.cs
@@ -5,28 +5,37 @@ public partial class Boot : Node
 {
     private int port = 3333;
     private int expectedClientCount = 2;
+    private int trackedClientCount = 0;
 
     public override void _Ready()
     {
+        ArmySyncer.instance.expectedClientCount = expectedClientCount;
+
         if(OS.HasFeature("Server"))
         {
-            InitializeServer();
-            Multiplayer.PeerConnected += (val) => {GD.Print(val + " connected");};
+            InitializeServer(expectedClientCount);
+            Multiplayer.PeerConnected += (val) => {BumpPlayerCount();};
         }
         else
         {
             LineEdit lineEdit = new LineEdit();
             AddChild(lineEdit);
-            lineEdit.CustomMinimumSize = new Vector2(1000, 80);
+            lineEdit.CustomMinimumSize = new Vector2(GetViewport().GetVisibleRect().Size.X, 80);
             lineEdit.PlaceholderText = "Server Ip";
             lineEdit.TextSubmitted += InitializeClient;
+
+            ArmySyncer.instance.AllClientNamesChosen += () => 
+            {
+                GD.Print(Multiplayer.GetUniqueId() + ": " + "Finished boot, go to new scene");
+                //Add scene transition here
+            };
         }
     }
 
-    private void InitializeServer()
+    private void InitializeServer(int maxClients)
     {
         var peer = new ENetMultiplayerPeer();
-        peer.CreateServer(port, expectedClientCount);
+        peer.CreateServer(port, maxClients);
         Multiplayer.MultiplayerPeer = peer;
         GD.Print("Initialized Server");
     }
@@ -37,5 +46,16 @@ public partial class Boot : Node
         peer.CreateClient(address, port);
         Multiplayer.MultiplayerPeer = peer;
         GD.Print("Initialized Client");
+    }
+
+    private void BumpPlayerCount()
+    {
+        trackedClientCount += 1;
+
+        if(trackedClientCount == expectedClientCount)
+        {
+            ArmySyncer.instance.AllClientsjoined();
+            GD.Print("All Clients joined");
+        }
     }
 }
