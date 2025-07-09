6f55d5d9fa37296ef76847765e6523a747d5ad78
Ethan-Schledewitz-Edwards <144922288+Ethan-Schledewitz-Edwards@users.noreply.github.com>
2025-07-09 13:37:15 -0400

    Enemy manager checkpoint

--- src/Enemies/Enemy_Scripts/EnemyManager.gd
+++ src/Enemies/Enemy_Scripts/EnemyManager.gd
@@ -1,11 +1,40 @@
 extends Node
 
+# Hard cap army sizes for performance
+const _maxArmySize : int = 256
 
-# Called when the node enters the scene tree for the first time.
+var armyAEnemy : Resource = preload("res://Enemies/Enemy_Prefabs/EnemyA.tscn")
+var armyBEnemy : Resource = preload("res://Enemies/Enemy_Prefabs/EnemyB.tscn")
+
 func _ready() -> void:
-	pass # Replace with function body.
+	
+	# Get networked values from signleton
+	var armyASize : int = 64
+	var armyBSize : int = 64
+	var seed : int = 64
+	
+	# Set RNG seed to ensure spawns are the same across both clients
+	seed(seed)
+	
+	_generateArmy(armyASize, true)
+	_generateArmy(armyBSize, false)
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func _generateArmy(armySize: int, isTeamA : bool) -> void:
+	
+	var fieldSide: int = 1 if isTeamA else -1
+	
+	var id : int = 0
+	for x in _maxArmySize / 2 * fieldSide:
+			for z in _maxArmySize / 2 * fieldSide:
+				if id <= armySize:
+					_spawnUnit(id, _getRandOffset(Vector2(x,z)))
+					id += 1
+
+func _spawnUnit(id: int, pos: Vector2) -> void:
+	pass
+
+func _getRandOffset(pos: Vector2) -> Vector2:
+	return Vector2.ZERO
