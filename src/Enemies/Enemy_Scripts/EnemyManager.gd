7f08ffa053c2b35e06cf7bacd55563ff1f88bd35
Ethan-Schledewitz-Edwards <144922288+Ethan-Schledewitz-Edwards@users.noreply.github.com>
2025-07-09 14:25:22 -0400

    Army with offsets
    THEY RANDOM

--- src/Enemies/Enemy_Scripts/EnemyManager.gd
+++ src/Enemies/Enemy_Scripts/EnemyManager.gd
@@ -1,10 +1,12 @@
 extends Node
 
-# Hard cap army sizes for performance
-const _maxArmySize : int = 256
 
-var armyAEnemy : Resource = preload("res://Enemies/Enemy_Prefabs/EnemyA.tscn")
-var armyBEnemy : Resource = preload("res://Enemies/Enemy_Prefabs/EnemyB.tscn")
+const _maxArmySize : int = 256 # Hard cap army sizes for performance
+const _unitSeparation : float = 2
+const _offsetFromCenter : float = 3 
+
+var _armyAUnit : Resource = preload("res://Enemies/Enemy_Prefabs/EnemyA.tscn")
+var _armyBUnit : Resource = preload("res://Enemies/Enemy_Prefabs/EnemyB.tscn")
 
 func _ready() -> void:
 	
@@ -26,20 +28,28 @@ func _generateArmy(armySize: int, isTeamA : bool) -> void:
 	
 	print("Generating army of : " + str(armySize) + " units")
 	
+	# Determine army size as a grid on the field
 	var fieldSide: int = 1 if isTeamA else -1
 	var gridSize = ceil(sqrt(armySize))
+	var offsetFromCenter = _offsetFromCenter * fieldSide
 	
+	# Create unit instances on each side of the field
 	var id : int = 0
 	for x in gridSize:
 			for z in gridSize:
 				if id <= armySize:
-					_spawnUnit(id, _getRandOffset(Vector3(x * fieldSide,0 ,z)))
+					_spawnUnit(id, 
+					_getRandOffset(Vector3(offsetFromCenter + (x * _unitSeparation) * fieldSide, 1, z * _unitSeparation))
+					)
+					
 					id += 1
 
 func _spawnUnit(id: int, pos: Vector3) -> void:
-	var instance = armyAEnemy.instantiate()
+	var instance = _armyAUnit.instantiate()
 	instance.position = pos
 	add_child(instance)
 
 func _getRandOffset(pos: Vector3) -> Vector3:
-	return pos
+	var offsetX : float = randf() * .5
+	var offsetZ : float = randf() * .5
+	return pos + Vector3(offsetX, 0 , offsetZ)
