99994b2f7e0ac0692aa5264daddd5d4e00a52163
Ethan-Schledewitz-Edwards <144922288+Ethan-Schledewitz-Edwards@users.noreply.github.com>
2025-07-10 16:13:06 -0400

    ECS Movement

--- src/Enemies/Enemy_Scripts/Unit.gd
+++ src/Enemies/Enemy_Scripts/Unit.gd
@@ -1,31 +1,14 @@
 class_name Unit
 
-extends CharacterBody3D
+extends Node3D
 
 # Stats
 const  _movementSpeed: float = 4
 
-# Components
-@onready var navAgent: NavigationAgent3D = $NavigationAgent3D
-
-# Signals
-signal OnTargetRequired(unit: Unit)
 signal OnDie()
-
-# System Vars
-var ID : int
-var _unitManager : UnitManager2
 
 enum HealthState { Healthy, Dazed, Injured }
 var currentState = HealthState.Healthy
-
-func Setup(id: int, unitManager: UnitManager2) -> void:
-	ID = id
-	_unitManager = unitManager
-
-# Called when this units target dies
-func _targetKilled()-> void:
-	OnTargetRequired.emit(self)
 
 # Increments enemy state each time function is called
 func _update_state():
@@ -35,8 +18,4 @@ func _update_state():
 	
 	if(currentState == HealthState.Injured):
 		OnDie.emit()
-
-func _process(delta: float) -> void:
-	var pos : Vector2 = _unitManager.units[ID]
-	position = Vector3(pos.x, 0, pos.y)
 	
--- src/Enemies/Enemy_Scripts/UnitManager2.gd
+++ src/Enemies/Enemy_Scripts/UnitManager2.gd
@@ -2,15 +2,10 @@ class_name UnitManager2
 
 extends Node
 
-signal OnBattleStart
-
 var units : PackedVector2Array
-var targets : PackedVector2Array
+var targets : PackedInt32Array
+var unitNodes : Array[Node3D]
 var armyASize: int
-
-# Hard cap army size for performance
-# This number *2 will be the max unit count on the field
-const _maxArmySize : int = 256 
 
 # Offset scalars
 const _unitSeparation : float = 2
@@ -26,23 +21,37 @@ func _ready() -> void:
 	var armyBSize : int = 64
 	var randSeed : int = 64
 	
+	var totalUnits: int = armyASize + armyBSize
+	
 	units.clear()
-	units.resize(armyASize + armyBSize)
+	units.resize(totalUnits)
 	
 	targets.clear()
-	targets.resize(armyASize + armyBSize)
+	targets.resize(totalUnits)
+	
+	unitNodes.clear()
+	unitNodes.resize(totalUnits)
 	
 	# Set RNG seed to ensure spawns are the same across both clients
 	seed(randSeed)
 	
 	# Generate the armies
 	_generateArmy(units.size())
-	
-	# Start the battle
-	OnBattleStart.emit()
 
 func _process(delta: float) -> void:
-	pass
+	# calculate positions
+	for n in range(0, units.size()):
+		var posfx : float = move_toward(units[n].x, 0, delta)
+		var posfy : float = move_toward(units[n].y, 0, delta)
+		var pos : Vector2 = Vector2(posfx, posfy)
+		units[n] = pos
+	
+	# send position data to units (node)
+	for n in range(0, units.size()):
+		var pos : Vector2 = units[n]
+		if unitNodes[n] == null:
+			continue
+		unitNodes[n].position = Vector3(pos.x, 0, pos.y)
 
 func _generateArmy(armySize: int) -> void:
 	
@@ -53,12 +62,15 @@ func _generateArmy(armySize: int) -> void:
 	# Determine army size as a grid on the field
 	var gridASize: int = ceil(sqrt(armyASize))
 	var gridBSize: int = ceil(sqrt(armyBSize))
+	
 	var aZOffset: float = gridASize * .5
 	var bZOffset: float = gridBSize * .5
 	
-	# Determine team
-	var fieldSide: int = 1 if armyASize else -1
-	var offsetFromCenter: float = _offsetFromCenter * fieldSide
+	var fieldSideA: int = 1 
+	var fieldSideB: int = -1
+	
+	var offsetFromCenterA: float = _offsetFromCenter * 1
+	var offsetFromCenterB: float = _offsetFromCenter * -1
 	
 	# Create unit instances on each side of the field
 	var id : int = 0
@@ -66,15 +78,15 @@ func _generateArmy(armySize: int) -> void:
 		for z in gridASize:
 			if id <= armyASize:
 				_spawnUnit(id,
-				_getRandOffset(Vector3(offsetFromCenter + (x * _unitSeparation) * fieldSide, 1, (aZOffset - z) * _unitSeparation)),)
+				_getRandOffset(Vector3(offsetFromCenterA + (x * _unitSeparation) * fieldSideA, 1, (aZOffset - z) * _unitSeparation)),)
 					
 				id += 1
 	id = 0
 	for x in gridBSize:
 		for z in gridBSize:
 			if id <= armyBSize:
-				_spawnUnit(id,
-				_getRandOffset(Vector3(offsetFromCenter + (x * _unitSeparation) * fieldSide, 1, (bZOffset - z) * _unitSeparation)),)
+				_spawnUnit(id + armyASize,
+				_getRandOffset(Vector3(offsetFromCenterB + (x * _unitSeparation) * fieldSideB, 1, (bZOffset - z) * _unitSeparation)),)
 					
 				id += 1
 
@@ -86,10 +98,9 @@ func _spawnUnit(id: int, pos: Vector3) -> void:
 	# Spawn and initialize the unit
 	var instance: Unit = unitMesh.instantiate()
 	instance.position = pos
-	instance.Setup(id, self)
+	units[id] = Vector2(pos.x, pos.z)
 	add_child(instance)
-	
-	instance.OnTargetRequired.connect(_onUnitRequireTarget)
+	unitNodes[id] = instance
 
 func _getRandOffset(pos: Vector3) -> Vector3:
 	
@@ -97,13 +108,13 @@ func _getRandOffset(pos: Vector3) -> Vector3:
 	var offsetZ : float = randf() * .5
 	return pos + Vector3(offsetX, 0 , offsetZ)
 
-func _onUnitRequireTarget(unit: Unit)-> void:
-	
-	var closestTarget: int = _findClosestUnit(unit.ID)
+func _onUnitRequireTarget(id: int)-> void:
+	var closestTarget: int = _findClosestUnit(id)
 	
 	# Set closest target
-	if !_isDead(closestTarget):
-		unit.SetTarget(closestTarget)
+	if ! _isDead(closestTarget):
+		targets[id] = closestTarget
+		
 	
 func _findClosestUnit(unitID: int) -> int:
 	var start: int = armyASize if unitID < armyASize else 0
