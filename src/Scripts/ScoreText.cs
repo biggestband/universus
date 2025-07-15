5af9b5c3aea59ac29dcf1cd04ea95829c0ed9a66
edamamet <adamtam2004@outlook.com>
2025-07-15 13:34:07 -0400

    combo feel

--- src/Scripts/ScoreText.cs
+++ src/Scripts/ScoreText.cs
@@ -2,16 +2,20 @@ using System;
 using Godot;
 
 public partial class ScoreText : RichTextLabel {
+    const int DEFAULT_FONT_SIZE = 75;
     Tween tween;
     Random random;
     Color startColor;
+    int currentValue;
+
     public override void _Ready() {
         random = new();
         startColor = Modulate;
         PachinkoEventManager.Instance.OnScore += UpdateScoreText;
         PachinkoEventManager.Instance.OnFinalScore += FinalScore;
         PachinkoEventManager.Instance.OnHit += TweenText;
-        UpdateScoreText(0,0);
+        UpdateScoreText(0, 0);
+        currentValue = DEFAULT_FONT_SIZE;
     }
     public override void _ExitTree() {
         PachinkoEventManager.Instance.OnScore -= UpdateScoreText;
@@ -21,12 +25,15 @@ public partial class ScoreText : RichTextLabel {
     void FinalScore(float baseScore, float multiplier) {
         var finalScore = baseScore * multiplier;
         Text = $"[b]{finalScore:F0}[/b]";
-        TweenText((int)HitType.NewHit);
+        tween?.Kill();
+        currentValue = 150;
+        TweenText((int)HitType.NewHit, 0.3f);
     }
     void UpdateScoreText(float oldScore, float newScore) {
         Text = $"[b]{newScore:F2}[/b]";
     }
-    void TweenText(int type) {
+    void TweenText(int type) => TweenText(type, 0.1f);
+    void TweenText(int type, float delay) {
         var hitType = (HitType)type;
         tween?.Kill();
         tween = CreateTween()
@@ -34,19 +41,24 @@ public partial class ScoreText : RichTextLabel {
             .SetTrans(Tween.TransitionType.Back)
             .SetParallel();
         var fontSize = hitType switch {
-            HitType.NewHit => 120,
-            _ => 80,
+            HitType.NewHit => 10,
+            _ => 5,
         };
         Color color = hitType switch {
             HitType.NewHit => new(0.5f, 1f, 0.5f),
-            _ => new(1,1,1),
+            _ => new(1, 1, 1),
         };
         const int POSITION_OFFSET = 20;
-        tween.TweenMethod(Callable.From<int>(ChangeFontSize), fontSize, 75, 0.5f);
+        currentValue += fontSize;
+        ChangeFontSize(currentValue);
+        tween.TweenMethod(Callable.From<int>(ChangeFontSize), currentValue, DEFAULT_FONT_SIZE, 0.5f).SetDelay(delay);
         tween.TweenProperty(this, "position", Vector2.Zero, 0.75f)
             .From(new Vector2(random.Next(-POSITION_OFFSET, POSITION_OFFSET), random.Next(-POSITION_OFFSET, POSITION_OFFSET)));
         Modulate = color;
         tween.TweenProperty(this, "modulate", startColor,  0.75).SetDelay(0.1f);
+        tween.TweenCallback(Callable.From(() => { currentValue = DEFAULT_FONT_SIZE; })).SetDelay(0.5f);
     }
-    void ChangeFontSize(int value) => AddThemeFontSizeOverride("bold_font_size", value);
+    void ChangeFontSize(int value) {
+        AddThemeFontSizeOverride("bold_font_size", value);
+    }
 }
